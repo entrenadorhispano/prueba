34b943c39e819257f7d5518f72b22ff78723ecef
entrenadorhispano <99187009+entrenadorhispano@users.noreply.github.com>
2022-03-04 09:12:48 -0400

    Add files via upload

---
+++ src/App copy.svelte
@@ -0,0 +1,122 @@
+<script>
+</script>
+
+<div class="navbar">
+</div>
+
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/K_Zc0_iTsfI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+
+<div class='column'>
+	<h1>Finalmente: Pierde 10 kg en 3 meses</h1>
+	<h2>(Los 7 pasos para tonificar el abdomen de manera permanente)</h2>
+	<img src="https://images.unsplash.com/photo-1558637845-c8b7ead71a3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8MTYlM0E5fGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="16:9">
+	<form action="https://calendly.com/proyectoshegua/nuevas_postulantes">
+		<button on:hover>
+			AGENDAR UNA LLAMADA GRATUITA de consulta. AHORA!
+			(valor: 50$)
+		</button>
+	</form>
+</div>
+
+
+
+<style>
+
+ 	@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap');
+
+	.navbar{
+		display: flex;
+		background-color: #ea384c;
+		width: 100%;
+		height: 30px;
+		margin-bottom: 25px;
+	}
+
+	.column{
+		display: flex;
+		flex-flow: column;
+		align-items: center;
+		background-color:rgb(22, 22, 22);
+		width: 90vw;
+		height: 100%;
+				
+	}
+
+	h1{
+     	width: 100%;
+		text-align: center;
+		font-size: 2em;
+		margin: 0px;
+		font-family: 'Nunito', sans-serif;
+		text-transform: uppercase;
+		font-weight: 900;
+		color: white;
+		letter-spacing: 0.7;
+	}
+	
+	h2{
+		display: none;
+	}
+
+	img{
+		width: 90%;
+		aspect-ratio: 16/9;
+		margin-top: 15px;
+	}
+
+	button{
+		margin-top: 15px;
+		margin-bottom: 15px;
+		background-color: #ea384c;
+		border-color: #ea384c;
+		border-radius: 25px;
+		font-family: 'Nunito', sans-serif;
+		font-weight: 700;
+		font-size: 2em;
+		color: white;
+		letter-spacing: 0.7;
+		padding: 15px;
+		font-size: 22px;
+	}
+	button:hover{
+		margin-top: 15px;
+		margin-bottom: 15px;
+		background-color: #ffffff;
+		border-color: #ea384c;
+		border-radius: 25px;
+		font-family: 'Nunito', sans-serif;
+		font-weight: 700;
+		font-size: 2em;
+		color: #ea384c;
+		letter-spacing: 0.7;
+		padding: 15px;
+		font-size: 22px;
+	}
+
+
+
+
+	@media screen and (min-width: 768px){ 
+
+	h2{
+		display: block;
+		background-color: #161616;
+		color: white;
+		width: 100%;
+		margin: 0px;
+		font-family: sans-serif;
+		text-align: center;
+		font-size: 22px;
+		
+	}
+
+	
+
+	}
+
+
+
+</style>
